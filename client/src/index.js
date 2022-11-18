4b4ce8e6728a7986136a21a33f83570abc0cf0e9
Enygma <miguelangelperezrefugio54@gmail.com>
2022-11-17 19:05:01 -0800

    antd

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { DatePicker, message } from 'antd';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [date, setDate] = useState(null);
+  const handleChange = value => {
+    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+    setDate(value);
+  };
+  return (
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
