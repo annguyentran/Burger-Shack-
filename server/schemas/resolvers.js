673bb8c052fff2db29e9b15aef4cf017a89bc5eb
Enygma <miguelangelperezrefugio54@gmail.com>
2022-11-21 20:06:53 -0800

    add user and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,59 @@ const resolvers = {
   },
 
   Mutation: {
-    // TODO: Add mutation
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+    
+      return {token, user};
+  },
+
+  login: async (parent, {email, password}) => {
+      const user = await User.findOne({email});
+
+      if(!user) {
+          throw new AuthenticationError('Incorrect Username/Password');
+      }
+
+      const correctPass = await user.isCorrectPassword(password);
+
+      if(!correctPass) {
+          throw new AuthenticationError('Incorrect Username/Password');
+      }
+
+      const token = signToken(user);
+      return {token, user};
+
+  },
+
+  saveOrder: async (parent, args, context) => {
+      if (context.user) {
+       const updatedUser =  await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedOrder: args.input } },
+          { new: true }
+        );
+    
+      return updatedUser;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+  },
+
+
+
+  removeOrder: async (parent, args, context) => {
+      if(context.user) {
+      const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedOrder: { orderId: args.orderId } } },
+          { new: true }
+      );
+
+      return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+  }
   }
 };
 
